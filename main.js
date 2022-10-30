395c3921caa609031516e6f075157119c7e42af0
sara sallat <sallatsara@gmail.com>
2022-10-30 14:57:41 +0300

    completed assignment

--- main.js
+++ main.js
@@ -2,11 +2,64 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
 // Your JavaScript code goes here!
+if (event.target.innerHTML === EMPTY_HEART) {
+  mimicServerCall().catch(() => displayError()).then(() => {
+    event.target.innerHTML = FULL_HEART;
+    event.target.setAttribute('class', 'like-glyph activated-heart');
+  });
+} else { function displayError() {
+  return new Promise((resolve,reject) => {
+    modal.removeAttribute('class');
+    setTimeout(() => {
+      modal.setAttribute('class','hidden');
+      resolve();
+    }, 3000);
+  });
+};
 
+function displayError(callback) {
+  modal.removeAttribute('class');
+  setTimeout(function(){
+      modal.setAttribute('class','hidden');
+      callback();
+  }, 3000);
+};
 
+function likifyMe(button) {
+  button.addEventListener('click', (event) => {
+    if (event.target.innerHTML === EMPTY_HEART) {
+      mimicServerCall().then(() => {
+          event.target.innerHTML = FULL_HEART;
+          event.target.setAttribute('class', 'like-glyph activated-heart');
+      }).catch(() => {
+          displayError(function(){
+              event.target.innerHTML = FULL_HEART;
+              event.target.setAttribute('class', 'like-glyph activated-heart');
+          });
+      });
+    } else {
+      event.target.innerHTML = EMPTY_HEART
+      event.target.setAttribute('class', 'like-glyph')
+    }
+  });
+};
 
-
+likeButtons.forEach(button => {
+  likifyMe(button)
+});
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
